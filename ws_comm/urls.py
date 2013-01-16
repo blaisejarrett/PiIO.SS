6f2f8786f50f90ec981c0a899de6c11c9d2fa6f4
Blaise Jarrett <me@blaisejarrett.com>
2013-01-16 11:44:08 -0800

    removed test url

--- ws_comm/urls.py
+++ ws_comm/urls.py
@@ -3,7 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^register/$', 'ws_comm.views.register'),
     url(r'^disconnect/$', 'ws_comm.views.disconnect'),
-
-    url(r'^test$', 'ws_comm.views.test'),
 )
 
